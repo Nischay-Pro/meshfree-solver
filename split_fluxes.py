6d81baa0a60325fe72e731ba1e1eeeac899bec45
Nischay Ram Mamidi <NischayPro@gmail.com>
2019-01-08 00:13:48 +0530

    init commit

---
+++ split_fluxes.py
@@ -0,0 +1,154 @@
+import math
+
+def flux_Gxp(Gxp, nx, ny, u1, u2, rho, pr):
+    tx = ny
+    ty = -nx
+
+    ut = u1*tx + u2*ty
+    un = u1*nx + u2*ny
+
+    beta = 0.5*rho/pr
+    S1 = ut*math.sqrt(beta) 
+    B1 = 0.5*math.exp(-S1*S1)/math.sqrt(math.pi*beta)
+    A1pos = 0.5*(1 + math.erf(S1))     
+
+    pr_by_rho = pr/rho
+    u_sqr = ut*ut + un*un
+
+    Gxp[1] = rho*(ut*A1pos + B1)  
+        
+    temp1 = pr_by_rho + ut*ut
+    temp2 = temp1*A1pos + ut*B1
+    Gxp[2] = rho*temp2
+
+    temp1 = ut*un*A1pos + un*B1
+    Gxp[3] = rho*temp1
+
+    temp1 = (7*pr_by_rho) + u_sqr
+    temp2 = 0.5*ut*temp1*A1pos 
+    temp1 = (6*pr_by_rho) + u_sqr
+    Gxp[4] = rho*(temp2 + 0.5*temp1*B1)
+
+def flux_Gxn(Gxn, nx, ny, u1, u2, rho, pr):
+    tx = ny
+    ty = -nx
+
+    ut = u1*tx + u2*ty
+    un = u1*nx + u2*ny
+
+    beta = 0.5*rho/pr
+    S1 = ut*math.sqrt(beta) 
+    B1 = 0.5*math.exp(-S1*S1)/math.sqrt(math.pi*beta)
+    A1neg = 0.5*(1 - math.erf(S1))     
+
+    pr_by_rho = pr/rho
+    u_sqr = ut*ut + un*un
+
+
+    Gxn[1] = rho*(ut*A1neg - B1)  
+
+    temp1 = pr_by_rho + ut*ut
+    temp2 = temp1*A1neg - ut*B1
+    Gxn[2] = rho*temp2
+
+    temp1 = ut*un*A1neg - un*B1
+    Gxn[3] = rho*temp1
+
+    temp1 = (7*pr_by_rho) + u_sqr
+    temp2 = 0.5*ut*temp1*A1neg 
+    temp1 = (6*pr_by_rho) + u_sqr
+    Gxn[4] = rho*(temp2 - 0.5*temp1*B1)
+
+def flux_Gyp(Gyp, nx, ny, u1, u2, rho, pr):
+    tx = ny
+    ty = -nx
+
+    ut = u1*tx + u2*ty
+    un = u1*nx + u2*ny
+
+    beta = 0.5*rho/pr
+    S2 = un*math.sqrt(beta) 
+    B2 = 0.5*math.exp(-S2*S2)/math.sqrt(math.pi*beta)
+    A2pos = 0.5*(1 + math.erf(S2))     
+
+    pr_by_rho = pr/rho
+    u_sqr = ut*ut + un*un
+
+
+    Gyp[1] = rho*(un*A2pos + B2)  
+
+    temp1 = pr_by_rho + un*un
+    temp2 = temp1*A2pos + un*B2
+    Gyp[3] = rho*temp2
+
+    temp1 = ut*un*A2pos + ut*B2
+    Gyp[2] = rho*temp1
+
+    temp1 = (7*pr_by_rho) + u_sqr
+    temp2 = 0.5*un*temp1*A2pos 
+    temp1 = (6*pr_by_rho) + u_sqr
+    Gyp[4] = rho*(temp2 + 0.5*temp1*B2)
+
+def flux_Gyn(Gyn, nx, ny, u1, u2, rho, pr):
+
+    tx = ny
+    ty = -nx
+
+    ut = u1*tx + u2*ty
+    un = u1*nx + u2*ny
+
+    beta = 0.5*rho/pr
+    S2 = un*math.sqrt(beta) 
+    B2 = 0.5*math.exp(-S2*S2)/math.sqrt(math.pi*beta)
+    A2neg = 0.5*(1 - math.erf(S2))     
+
+    pr_by_rho = pr/rho
+    u_sqr = ut*ut + un*un
+
+    Gyn[1] = rho*(un*A2neg - B2)  
+    
+    temp1 = pr_by_rho + un*un
+    temp2 = temp1*A2neg - un*B2
+    Gyn[3] = rho*temp2
+
+    temp1 = ut*un*A2neg - ut*B2
+    Gyn[2] = rho*temp1
+
+    temp1 = (7*pr_by_rho) + u_sqr
+    temp2 = 0.5*un*temp1*A2neg 
+    temp1 = (6*pr_by_rho) + u_sqr
+    Gyn[4] = rho*(temp2 - 0.5*temp1*B2)
+
+def flux_Gx(Gx, nx, ny, u1, u2, rho, pr):
+    tx = ny
+    ty = -nx
+
+    ut = u1*tx + u2*ty
+    un = u1*nx + u2*ny
+
+    Gx[1] = rho*ut
+
+    Gx[2] = pr + rho*ut*ut
+
+    Gx[3] = rho*ut*un
+
+    temp1 = 0.5*(ut*ut + un*un)
+    rho_e = 2.5*pr + rho*temp1
+    Gx[4] = (pr + rho_e)*ut
+
+def flux_Gy(Gy, nx, ny, u1, u2, rho, pr):
+    tx = ny
+    ty = -nx
+
+    ut = u1*tx + u2*ty
+    un = u1*nx + u2*ny
+
+    Gy[1] = rho*un
+
+    Gy[2] = rho*ut*un
+
+    Gy[3] = pr + rho*un*un
+
+    temp1 = 0.5*(ut*ut + un*un)
+    rho_e = 2.5*pr + rho*temp1
+    Gy[4] = (pr + rho_e)*un
