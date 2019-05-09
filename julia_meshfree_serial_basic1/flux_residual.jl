641043f75d28be590cf83a4c38e42488f89bee08
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-05-09 10:49:53 +0530

    Add Julia folder

---
+++ julia_meshfree_serial_basic1/flux_residual.jl
@@ -0,0 +1,46 @@
+function cal_flux_residual(globaldata, wallindices, outerindices, interiorindices, configData)
+	max_q = Vector{Float64}(undef, 1)
+	min_q = Vector{Float64}(undef, 1)
+	wallindices_flux_residual(globaldata, configData, wallindices, max_q, min_q)
+	outerindices_flux_residual(globaldata, configData, outerindices, max_q, min_q)
+	interiorindices_flux_residual(globaldata, configData, interiorindices, max_q, min_q)
+end
+
+function wallindices_flux_residual(globaldata, configData, wallindices, max_q, min_q)
+	for itm in wallindices
+		Gxp = wall_dGx_pos(globaldata, itm, configData, max_q, min_q)
+		Gxn = wall_dGx_neg(globaldata, itm, configData, max_q, min_q)
+		Gyn = wall_dGy_neg(globaldata, itm, configData, max_q, min_q)
+		GTemp = Gxp + Gxn + Gyn
+		# if itm == 100
+		# 	println("=======")
+		# 	println(IOContext(stdout, :compact => false), Gxp)
+		# 	println(IOContext(stdout, :compact => false), Gxn)
+		# 	println(IOContext(stdout, :compact => false), Gyn)
+		# 	println()
+		# end
+		GTemp = GTemp * 2.0
+		globaldata[itm].flux_res = GTemp
+	end
+end
+
+function outerindices_flux_residual(globaldata, configData, outerindices, max_q, min_q)
+	for itm in outerindices
+		Gxp = outer_dGx_pos(globaldata, itm, configData, max_q, min_q)
+		Gxn = outer_dGx_neg(globaldata, itm, configData, max_q, min_q)
+		Gyp = outer_dGy_pos(globaldata, itm, configData, max_q, min_q)
+		GTemp = Gxp + Gxn + Gyp
+		globaldata[itm].flux_res = GTemp
+	end
+end
+
+function interiorindices_flux_residual(globaldata, configData, interiorindices, max_q, min_q)
+	for itm in interiorindices
+		Gxp = interior_dGx_pos(globaldata, itm, configData, max_q, min_q)
+		Gxn = interior_dGx_neg(globaldata, itm, configData, max_q, min_q)
+		Gyp = interior_dGy_pos(globaldata, itm, configData, max_q, min_q)
+		Gyn = interior_dGy_neg(globaldata, itm, configData, max_q, min_q)
+		GTemp = @.(Gxp + Gxn + Gyp + Gyn)
+		globaldata[itm].flux_res = GTemp
+	end
+end
---
+++ julia_meshfree_serial_basic1/main_module.jl
@@ -0,0 +1,53 @@
+__precompile__()
+
+module main_module
+
+using Printf
+# using PyCall
+
+# const math = PyNULL()
+
+# function __init__()
+#     copy!(math, pyimport("math"))
+# end
+
+include("config.jl")
+export getConfig
+
+include("core.jl")
+export getInitialPrimitive, getInitialPrimitive2, calculateNormals, calculateConnectivity, fpi_solver, q_var_derivatives
+
+include("flux_residual.jl")
+export cal_flux_residual, wallindices_flux_residual, outerindices_flux_residual, interiorindices_flux_residual
+
+include("interior_fluxes.jl")
+export interior_dGx_pos, interior_dGx_neg, interior_dGy_pos, interior_dGy_neg
+
+include("limiters.jl")
+export venkat_limiter, maximum, minimum, smallest_dist, min_q_values, qtilde_to_primitive
+
+include("meshfree_solver.jl")
+export main
+
+include("objective_function.jl")
+export calculateTheta, compute_cl_cd_cm
+
+include("outer_fluxes.jl")
+export outer_dGx_pos, outer_dGx_neg, outer_dGy_pos
+
+include("point.jl")
+export Point, setNormals, getxy, setConnectivity
+
+include("quadrant_fluxes.jl")
+export flux_quad_GxI, flux_quad_GxII, flux_quad_GxIII, flux_quad_GxIV
+
+include("split_fluxes.jl")
+export flux_Gxp, flux_Gxn, flux_Gyp, flux_Gyn, flux_Gx, flux_Gy
+
+include("state_update.jl")
+export func_delta, state_update, primitive_to_conserved, conserved_vector_Ubar
+
+include("wall_fluxes.jl")
+export wall_dGx_pos, wall_dGx_neg, wall_dGy_neg
+
+end
