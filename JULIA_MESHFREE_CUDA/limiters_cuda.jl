e84345d6f3a69ad6c1cdb3973a956ecfd77abce5
TestSubjector <f2015845@hyderabad.bits-pilani.ac.in>
2019-05-27 18:10:46 +0530

    Prep for file format change

--- JULIA_MESHFREE_CUDA/limiters_cuda.jl
+++ JULIA_MESHFREE_CUDA/limiters_cuda.jl
@@ -188,6 +188,17 @@ end
     return nothing
 end
 
+@inline function smallest_dist(globaldata, idx::Int64)
+    min_dist = 1000.0
+    for itm in globaldata[idx].conn
+        ds = hypot(globaldata[idx].x - globaldata[itm].x, globaldata[idx].y - globaldata[itm].y)
+        if ds < min_dist
+            min_dist = ds
+        end
+    end
+    globaldata[idx].short_distance = min_dist
+end
+
 @inline function qtilde_to_primitive_kernel(qtilde, gpuConfigData, gpuGlobalDataCommon, idx)
 
     gamma = gpuConfigData[15]
--- JULIA_MESHFREE_CUDA/main_module.jl
+++ JULIA_MESHFREE_CUDA/main_module.jl
@@ -18,30 +18,18 @@ using CUDAdrv
 include("config.jl")
 export getConfig
 
-# include("core.jl")
-# export getInitialPrimitive, getInitialPrimitive2, calculateNormals, calculateConnectivity, fpi_solver, q_var_derivatives
-
 include("core_cuda.jl")
 export getInitialPrimitive, getInitialPrimitive2, calculateNormals, calculateConnectivity,
 fpi_solver_cuda, q_var_cuda_kernel, q_var_derivatives_kernel, max_min_kernel
 
-# include("flux_residual.jl")
-# export cal_flux_residual, wallindices_flux_residual, outerindices_flux_residual, interiorindices_flux_residual
-
 include("flux_residual_cuda.jl")
 export cal_flux_residual_kernel
-
-# include("interior_fluxes.jl")
-# export interior_dGx_pos, interior_dGx_neg, interior_dGy_pos, interior_dGy_neg
 
 include("interior_fluxes_cuda.jl")
 export interior_dGx_pos_kernel, interior_dGx_neg_kernel, interior_dGy_pos_kernel, interior_dGy_neg_kernel
 
-include("limiters.jl")
-export venkat_limiter, maximum, minimum, smallest_dist, min_q_values, qtilde_to_primitive
-
 include("limiters_cuda.jl")
-export venkat_limiter_kernel_i, venkat_limiter_kernel_k
+export venkat_limiter_kernel_i, venkat_limiter_kernel_k, qtilde_to_primitive_kernel
 
 include("meshfree_solver.jl")
 export main
@@ -52,35 +40,20 @@ export calculateTheta, compute_cl_cd_cm
 include("objective_function_cuda.jl")
 export compute_cl_cd_cm_kernel, calculateTheta
 
-# include("outer_fluxes.jl")
-# export outer_dGx_pos, outer_dGx_neg, outer_dGy_pos
-
 include("outer_fluxes_cuda.jl")
 export outer_dGx_pos_kernel, outer_dGx_neg_kernel, outer_dGy_pos_kernel
 
 include("point.jl")
 export Point, setNormals, getxy, setConnectivity, convertToArray
 
-# include("quadrant_fluxes.jl")
-# export flux_quad_GxI, flux_quad_GxII, flux_quad_GxIII, flux_quad_GxIV
-
 include("quadrant_fluxes_cuda.jl")
 export flux_quad_GxI_kernel, flux_quad_GxII_kernel, flux_quad_GxIII_kernel, flux_quad_GxIV_kernel
-
-# include("split_fluxes.jl")
-# export flux_Gxp, flux_Gxn, flux_Gyp, flux_Gyn, flux_Gx, flux_Gy
 
 include("split_fluxes_cuda.jl")
 export flux_Gxp_kernel, flux_Gxn_kernel, flux_Gyp_kernel, flux_Gyn_kernel, flux_Gx_kernel, flux_Gy_kernel
 
-# include("state_update.jl")
-# export func_delta, state_update, primitive_to_conserved, conserved_vector_Ubar
-
 include("state_update_cuda.jl")
 export func_delta_kernel, state_update_kernel
-
-# include("wall_fluxes.jl")
-# export wall_dGx_pos, wall_dGx_neg, wall_dGy_neg
 
 include("wall_fluxes_cuda.jl")
 export wall_dGx_pos_kernel, wall_dGx_neg_kernel, wall_dGy_neg_kernel
