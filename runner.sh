3d63661a821c1ba2410f4ffb9192db920ceb530b
Nischay Ram Mamidi <NischayPro@gmail.com>
2019-06-01 14:51:45 +0530

    cleanup

--- runner.sh
+++
@@ -1,4 +0,0 @@
-python3 meshfree_solver.py -f ../meshfree-solver_cuda/grids/partGridNew--640-240 -t 32
-python3 meshfree_solver.py -f ../meshfree-solver_cuda/grids/partGridNew--1280-480 -t 32
-python3 meshfree_solver.py -f ../meshfree-solver_cuda/grids/partGridNew--2560-960 -t 32
-python3 meshfree_solver.py -f ../meshfree-solver_cuda/grids/partGridNew--5120-1920 -t 32
