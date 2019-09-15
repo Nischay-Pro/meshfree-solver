b3bfbe9ff25e1222958fc468f59d646ff32a8ee2
Nischay Ram Mamidi <NischayPro@gmail.com>
2019-09-15 10:27:52 +0530

    cuthill updated

--- cuthill.py
+++ cuthill.py
@@ -16,7 +16,11 @@ def main():
     args = parser.parse_args()
 
     file_length = int(subprocess.run(['wc', '-l', args.input], stdout=subprocess.PIPE).stdout.decode('utf-8').split(" ")[0])
-    G.add_nodes_from(range(1,file_length + 1))
+    if args.legacy == 1:
+        G.add_nodes_from(range(1,file_length + 1))
+    else:
+        G.add_nodes_from(range(1,file_length))
+    
     data = []
     data_new = []
 
@@ -36,28 +40,47 @@ def main():
                 connectivity_set = list(map(int, clean_line[8:]))
                 connectivity_set = list(zip_with_scalar(connectivity_set, row + 1))
                 G.add_edges_from(connectivity_set)
-        print("Applying Cuthill McKee")
-        if args.heuristics == 0:
-            result = list(cuthill_mckee_ordering(G))        
-        elif args.heuristics == 1:
-            result = list(cuthill_mckee_ordering(G, heuristic=smallest_degree))
-        elif args.heuristics == 2:
-            result = list(cuthill_mckee_ordering(G, heuristic=biggest_degree))
-        else:
-            exit()
-        print("Converting Data Type")
-        renumbering_matrix = list(zip(range(1, file_length + 1), result))
-        for item in tqdm(data):
-            temp = [result[itm - 1] if isinstance(itm, int) else itm for itm in item]
-            data_new.append(temp)
-        renumbering_matrix = sorted(renumbering_matrix, key=lambda tup: tup[1])
-        with open(args.output, "w+") as fileobject:
-            for itm in renumbering_matrix:
-                idx = itm[0]
-                temp = data_new[idx - 1]
-                temp = " ".join(list(map(str, temp)))
-                fileobject.write(temp + "\n")
-        print("Done")
+
+    else:
+        with open(args.input) as fileobject:
+            for row, line in enumerate(tqdm(fileobject, total=file_length)):
+                if row > 0:
+                    line = line.replace("\n","")
+                    clean_line = line.split(" ")
+                    clean_line.pop(-1)
+                    temp = []
+                    for idx, itm in enumerate(clean_line):
+                        if idx > 7:
+                            itm = int(itm)
+                        temp.append(itm)
+                    data.append(temp)
+                    connectivity_set = list(map(int, clean_line[8:]))
+                    connectivity_set = list(zip_with_scalar(connectivity_set, row))
+                    G.add_edges_from(connectivity_set)
+    
+    print("Applying Cuthill McKee")
+    if args.heuristics == 0:
+        result = list(cuthill_mckee_ordering(G))        
+    elif args.heuristics == 1:
+        result = list(cuthill_mckee_ordering(G, heuristic=smallest_degree))
+    elif args.heuristics == 2:
+        result = list(cuthill_mckee_ordering(G, heuristic=biggest_degree))
+    else:
+        exit()
+    print("Converting Data Type")
+    renumbering_matrix = list(zip(range(1, file_length + 1), result))
+    for item in tqdm(data):
+        temp = [result[itm - 1] if isinstance(itm, int) else itm for itm in item]
+        data_new.append(temp)
+    renumbering_matrix = sorted(renumbering_matrix, key=lambda tup: tup[1])
+    with open(args.output, "w+") as fileobject:
+        fileobject.write("{}\n".format(file_length - 1))
+        for itm in renumbering_matrix:
+            idx = itm[0]
+            temp = data_new[idx - 1]
+            temp = " ".join(list(map(str, temp)))
+            fileobject.write(temp + "\n")
+    print("Done")
 
     # print(renumbering_matrix)
 
